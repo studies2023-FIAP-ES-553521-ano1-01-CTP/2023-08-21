c8683b5c46f7f25cbc13f6db1c313d36c55b0c6b
Rafael Cristofali <79152217+Rafafaaa@users.noreply.github.com>
2023-10-18 19:58:44 -0300

    Create If e Else.py

---
+++ If e Else.py
@@ -0,0 +1,27 @@
+#--> If e Else
+#--> if (executa algo se os operadores booleanos retornarem True)
+#--> else (executa algo se não entrar no if)
+# nota = 6
+# if (nota >= 5):
+#     print('Aprovado')
+# else:
+#     print('Reprovado') 
+ 
+#--> elif (ou else if, é o else porém cria um novo if)
+# nota = 2
+# if nota >= 6:
+#     print('aprovado')
+# elif nota >= 4:
+#     print('exame')
+# else:
+#     print('reprovado')
+
+#--> Utilizando if e else com operadores booleanos
+# deficiente = input("Você possui alguma deficiência? ")
+# idoso = input("Você é idoso? ")
+# cartao = input("Você possui cartão? ")
+# 
+# if ((deficiente == 'sim' or idoso == 'sim') and cartao == 'sim'):
+#     print('Pode estacionar')
+# else:
+#     print('Procure outra vaga')
